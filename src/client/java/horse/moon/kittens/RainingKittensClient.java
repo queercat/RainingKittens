c0cc2fca0b6a5edb53e1f5206504c2afbc14e162
May Tusek <tusek.may@gmail.com>
2024-11-15 16:03:58 -0800

    Adds mod.

---
+++ src/client/java/horse/moon/kittens/RainingKittensClient.java
@@ -0,0 +1,10 @@
+package horse.moon.kittens;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RainingKittensClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/horse/moon/kittens/RainingKittens.java
@@ -0,0 +1,45 @@
+package horse.moon.kittens;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+
+public class RainingKittens implements ModInitializer {
+	public static final String MOD_ID = "raining-kittens";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ServerTickEvents.END_SERVER_TICK.register(server -> {
+			for (ServerWorld world : server.getWorlds()) {
+				world.getPlayers().forEach(player -> {
+					if (server.getTicks() % 20 == 0) {
+						MakeItRain(world, player.getBlockPos().add(0, 10, 0), 4);
+					}
+
+					if (player.getAbilities().flying) {
+						world.spawnParticles(ParticleTypes.CLOUD, player.getX(), player.getY(), player.getZ(), 10, .5, .5, .5, .05);
+					}
+				});
+			}
+		});
+	}
+
+	private void MakeItRain(ServerWorld world, BlockPos position, int maxNumber) {
+		var random = new Random();
+
+		for (var i = 0; i < Math.max(random.nextInt(maxNumber), 1); ++i) {
+			EntityType.CAT.spawn(world, position.add(random.nextInt(4), 0, random.nextInt(4)), SpawnReason.COMMAND);
+		}
+	}
+}
